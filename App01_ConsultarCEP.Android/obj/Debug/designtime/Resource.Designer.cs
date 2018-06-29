fc9e06b0bb5db31adf305f63ca4fa4f2bcb40f6f
Luciano <luciano.gusso@gmail.com>
2018-06-29 13:06:00 -0300

    App concluido
    app concluido

--- App01_ConsultarCEP.Android/obj/Debug/designtime/Resource.Designer.cs
+++ App01_ConsultarCEP.Android/obj/Debug/designtime/Resource.Designer.cs
@@ -27,17 +27,17 @@ namespace App01_ConsultarCEP.Droid
 		public partial class Color
 		{
 			
-			// aapt resource value: 0
-			public static int colorAccent = 0;
+			// aapt resource value: 0x7F050005
+			public const int colorAccent = 2131034117;
 			
-			// aapt resource value: 0
-			public static int colorPrimary = 0;
+			// aapt resource value: 0x7F050003
+			public const int colorPrimary = 2131034115;
 			
-			// aapt resource value: 0
-			public static int colorPrimaryDark = 0;
+			// aapt resource value: 0x7F050004
+			public const int colorPrimaryDark = 2131034116;
 			
-			// aapt resource value: 0
-			public static int launcher_background = 0;
+			// aapt resource value: 0x7F050002
+			public const int launcher_background = 2131034114;
 			
 			static Color()
 			{
@@ -49,14 +49,30 @@ namespace App01_ConsultarCEP.Droid
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7F010002
+			public const int logo_correios = 2130771970;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0
-			public static int sliding_tabs = 0;
+			// aapt resource value: 0x7F020002
+			public const int sliding_tabs = 2130837506;
 			
-			// aapt resource value: 0
-			public static int toolbar = 0;
+			// aapt resource value: 0x7F020003
+			public const int toolbar = 2130837507;
 			
 			static Id()
 			{
@@ -71,11 +87,11 @@ namespace App01_ConsultarCEP.Droid
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0
-			public static int Tabbar = 0;
+			// aapt resource value: 0x7F030002
+			public const int Tabbar = 2130903042;
 			
-			// aapt resource value: 0
-			public static int Toolbar = 0;
+			// aapt resource value: 0x7F030003
+			public const int Toolbar = 2130903043;
 			
 			static Layout()
 			{
@@ -90,14 +106,14 @@ namespace App01_ConsultarCEP.Droid
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0
-			public static int icon = 0;
+			// aapt resource value: 0x7F040002
+			public const int icon = 2130968578;
 			
-			// aapt resource value: 0
-			public static int icon_round = 0;
+			// aapt resource value: 0x7F040003
+			public const int icon_round = 2130968579;
 			
-			// aapt resource value: 0
-			public static int launcher_foreground = 0;
+			// aapt resource value: 0x7F040004
+			public const int launcher_foreground = 2130968580;
 			
 			static Mipmap()
 			{
@@ -112,14 +128,14 @@ namespace App01_ConsultarCEP.Droid
 		public partial class Style
 		{
 			
-			// aapt resource value: 0
-			public static int AppCompatDialogStyle = 0;
+			// aapt resource value: 0x7F060004
+			public const int AppCompatDialogStyle = 2131099652;
 			
-			// aapt resource value: 0
-			public static int MainTheme = 0;
+			// aapt resource value: 0x7F060002
+			public const int MainTheme = 2131099650;
 			
-			// aapt resource value: 0
-			public static int MainTheme_Base = 0;
+			// aapt resource value: 0x7F060003
+			public const int MainTheme_Base = 2131099651;
 			
 			static Style()
 			{
--- App01_ConsultarCEP/MainPage.xaml.cs
+++ App01_ConsultarCEP/MainPage.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using App01_ConsultarCEP.Servico.Modelo;
+using App01_ConsultarCEP.Servico;
 
 namespace App01_ConsultarCEP
 {
@@ -12,6 +11,61 @@ namespace App01_ConsultarCEP
         public MainPage()
         {
             InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - Lógica do programa
+
+            //Validações;
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereco: {2}, Bairro: {3}, {0},{1}",
+                                               end.localidade, end.uf, end.logradouro,
+                                               end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado: " + end, "OK");   
+                    }
+                } 
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRITICO", e.Message, "OK");
+                }
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP inválideo! O CEP deve conter 8 carcteres.", "OK");
+
+                valido = false;
+            }
+
+            int NovoCEP = 0;
+            if (!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("ERRO", "CEP inválideo! O CEP deve ser composto apenas por númeross.", "OK");
+
+                valido = false;
+
+            }
+            return valido;
         }
     }
 }
